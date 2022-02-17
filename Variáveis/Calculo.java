bda56a4982d6d7c96fbf1121102a48d8e149cc32
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2022-02-17 08:45:26 -0300

    Variáveis

---
+++ Variáveis/Calculo.java
@@ -0,0 +1,20 @@
+public class Calculo {
+    public static void main(String[] args) {
+
+        int num1 = 50; //Atribui 50 a variável num1
+        int num2 = 25; //Atribui 25 a variável num2
+
+        int num3 = num1 - num2; //Atribui a subtração de num1 por num2 a variável num3
+        int num4 = num1 + num1; //Atribui a soma de num1 por num1 a variável num4
+        int num5 = num1 * num2; //Atribui a multiplicação de num1 por num2 a variável num5
+        int num6 = num1 / num2; //Atribui a divisão de num1 por num2 a variável num6
+
+
+        //Imprime as respectivas variáveis
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+
+    }
+}
---
+++ Variáveis/Variavel.java
@@ -0,0 +1,21 @@
+public class Variavel {
+    public static void main(String[] args) {
+        int numero = 5; //Variável que armazena inteiros
+        String palavra = "Rato"; //Variável que armazena palavras (sequência de caractéres)
+        float z = 2.6f; //Variável que armazena números decimais
+        double z2 = 2.8; //Variável que armazena números decimais
+        char letra = 'A'; //Variável que armazena um único caracter
+        long numeroGrande = 123187672; //Variável que armazena valores grandes
+        boolean status = true; //Variável que armazena verdadeiro ou falso
+
+        //Imrpime o texto e os valores das variáveis respectivas
+        System.out.println("Variável inteira: " + numero); 
+        System.out.println("Variável string: " + palavra);
+        System.out.println("Variável float: " + z);
+        System.out.println("Variável double: " + z2);
+        System.out.println("Variável char: " + letra);
+        System.out.println("Variável long: " + numeroGrande);
+        System.out.println("Variável boolean: " + status);
+
+    }
+}
